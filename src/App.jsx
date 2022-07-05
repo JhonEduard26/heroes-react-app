697a674dfb56ac0846234a15e321d3c520dbe3c7
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-07-05 17:04:24 -0500

    [ADD] reducer and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useReducer } from 'react'
 import './App.css'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false }
+}
+
 function App() {
-  return <AppRouter />
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <AppRouter />
+    </AuthContext.Provider>
+  )
 }
 
 export default App
